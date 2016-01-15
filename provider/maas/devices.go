52d1dd1c1631f687388181b542cdbc2fc8b8fa39
Dimiter Naydenov <dimiter.naydenov@canonical.com>
2016-01-15 20:04:28 +0200

    Finished stub-based tests for all but 2 of the device APIs

--- provider/maas/devices.go
+++ provider/maas/devices.go
@@ -76,19 +76,79 @@ func parseDevices(jsonData json.Marshaler) ([]maasDevice, error) {
 // devices API calls.
 type devicesAPI interface {
 	GetSubObject(uri string) devicesAPI
-	CallGet(op string, params url.Values) (gomaasapi.JSONObject, error)
-	CallPost(op string, params url.Values) (gomaasapi.JSONObject, error)
+	CallGet(op string, params url.Values) (devicesAPI, error)
+	CallPost(op string, params url.Values) (devicesAPI, error)
 	Delete() error
-	Update(params url.Values) (gomaasapi.JSONObject, error)
-	Get() (gomaasapi.JSONObject, error)
+	Update(params url.Values) (devicesAPI, error)
+	Get() (devicesAPI, error)
+	MarshalJSON() ([]byte, error)
 }
 
-var _ devicesAPI = (*gomaasapi.MAASObject)(nil)
+// devicesAPIShim adapts both gomaasapi.MAASObject and gomaasapi.JSONObject to
+// devicesAPI interface, as devicesAPI uses different return types, which can't
+// be changed.
+//
+// TODO(dimitern): Drop this once the gomaasapi test server allows inspecting
+// requests/responses and/or supports better the needed API calls.
+//
+// LKK Card: https://canonical.leankit.com/Boards/View/101652562/119310616
+type devicesAPIShim struct {
+	*gomaasapi.MAASObject
+	*gomaasapi.JSONObject
+}
 
-// devicesClient returns the MAASObject to use when calling devices API calls
+func (d *devicesAPIShim) GetSubObject(uri string) devicesAPI {
+	obj := d.MAASObject.GetSubObject(uri)
+	return &devicesAPIShim{MAASObject: &obj}
+}
+
+func (d *devicesAPIShim) CallGet(op string, params url.Values) (devicesAPI, error) {
+	result, err := d.MAASObject.CallGet(op, params)
+	if err != nil {
+		return nil, errors.Trace(err)
+	}
+	return &devicesAPIShim{JSONObject: &result}, nil
+}
+
+func (d *devicesAPIShim) CallPost(op string, params url.Values) (devicesAPI, error) {
+	result, err := d.MAASObject.CallPost(op, params)
+	if err != nil {
+		return nil, errors.Trace(err)
+	}
+	return &devicesAPIShim{JSONObject: &result}, nil
+}
+
+func (d *devicesAPIShim) Update(params url.Values) (devicesAPI, error) {
+	result, err := d.MAASObject.Update(params)
+	if err != nil {
+		return nil, errors.Trace(err)
+	}
+	return &devicesAPIShim{MAASObject: &result}, nil
+}
+
+func (d *devicesAPIShim) Get() (devicesAPI, error) {
+	result, err := d.MAASObject.Get()
+	if err != nil {
+		return nil, errors.Trace(err)
+	}
+	return &devicesAPIShim{MAASObject: &result}, nil
+}
+
+func (d *devicesAPIShim) MarshalJSON() ([]byte, error) {
+	if d.JSONObject != nil {
+		return d.JSONObject.MarshalJSON()
+	}
+	if d.MAASObject != nil {
+		return d.MAASObject.MarshalJSON()
+	}
+	return nil, errors.Errorf("no object available to marshal")
+}
+
+// devicesClient returns the devicesAPI to use when calling devices API calls
 // and allows to test the requests and responses of those calls.
-var devicesClient = func(environ *maasEnviron) gomaasapi.MAASObject {
-	return environ.getMAASClient().GetSubObject("devices")
+var devicesClient = func(environ *maasEnviron) devicesAPI {
+	obj := environ.getMAASClient().GetSubObject("devices")
+	return &devicesAPIShim{MAASObject: &obj}
 }
 
 // listDevices returns the parsed devices list, which match all of the
@@ -169,9 +229,9 @@ func (environ *maasEnviron) updateDevice(device maasDevice) error {
 		params.Add("parent", *device.ParentID)
 	}
 
-	_, err := deviceObj.Update("update", params)
+	_, err := deviceObj.Update(params)
 	if code := getMAASErrorCode(err); code == 404 {
-		return errors.NotFoundf("device %d", device.SystemID)
+		return errors.NotFoundf("device %q", device.SystemID)
 	} else if err != nil {
 		return errors.Annotatef(err, "cannot update device %q", device.SystemID)
 	}
@@ -186,7 +246,7 @@ func (environ *maasEnviron) deleteDevice(deviceID string) error {
 
 	err := deviceObj.Delete()
 	if code := getMAASErrorCode(err); code == 404 {
-		return errors.NotFoundf("device %d", deviceID)
+		return errors.NotFoundf("device %q", deviceID)
 	} else if err != nil {
 		return errors.Annotatef(err, "cannot delete device %q", deviceID)
 	}
@@ -199,9 +259,9 @@ func (environ *maasEnviron) deleteDevice(deviceID string) error {
 func (environ *maasEnviron) getDevice(deviceID string) (*maasDevice, error) {
 	deviceObj := devicesClient(environ).GetSubObject(deviceID)
 
-	result, err := device.Get()
+	result, err := deviceObj.Get()
 	if code := getMAASErrorCode(err); code == 404 {
-		return nil, errors.NotFoundf("device %d", deviceID)
+		return nil, errors.NotFoundf("device %q", deviceID)
 	} else if err != nil {
 		return nil, errors.Annotatef(err, "cannot get device %q", deviceID)
 	}
--- provider/maas/devices_test.go
+++ provider/maas/devices_test.go
@@ -5,24 +5,13 @@ package maas
 
 import (
 	"encoding/json"
-	"fmt"
-	"net/http"
-	"net/http/httptest"
-	"net/http/httputil"
 	"net/url"
 
-	"launchpad.net/gomaasapi"
-
+	"github.com/juju/errors"
 	"github.com/juju/testing"
 	jc "github.com/juju/testing/checkers"
 	gc "gopkg.in/check.v1"
 )
-
-type devicesSuite struct {
-	providerSuite
-}
-
-var _ = gc.Suite(&devicesSuite{})
 
 const (
 	exampleSingleDeviceNoParentJSON = `
@@ -120,6 +109,42 @@ var (
 	}
 )
 
+type devicesSuite struct {
+	providerSuite
+
+	fakeAPI      *fakeDevicesAPI
+	maas404Error error
+}
+
+var _ = gc.Suite(&devicesSuite{})
+
+func (s *devicesSuite) SetUpTest(c *gc.C) {
+	s.providerSuite.SetUpTest(c)
+
+	fakeDevices := []maasDevice{
+		expectedSingleDeviceNoParent,
+		expectedSingleDeviceWithParent,
+	}
+
+	s.fakeAPI = &fakeDevicesAPI{
+		Stub:         &testing.Stub{},
+		allDevices:   fakeDevices,
+		singleDevice: fakeDevices[0],
+	}
+	s.PatchValue(&devicesClient, func(environ *maasEnviron) devicesAPI {
+		c.Assert(environ, gc.NotNil)
+		return s.fakeAPI
+	})
+
+	// Since we can't set the embedded error interface inside
+	// gomaasapi.ServerError as it's "unexported", we trick the test server to
+	// do it instead. We need to test the behavior when 404 is received. Also
+	// Due to the awkward multi-line error message formatting + parenthesis in
+	// ServerError it's simpler to use HasPrefix instead of ErrorMatches.
+	s.maas404Error = s.testMAASObject.Delete()
+	c.Assert(s.maas404Error.Error(), jc.HasPrefix, "gomaasapi: got error back from server: 404 Not Found")
+}
+
 func (s *devicesSuite) TestParseDevicesNoJSON(c *gc.C) {
 	result, err := parseDevices(nil)
 	c.Check(err, gc.ErrorMatches, "nil JSON data")
@@ -168,65 +193,315 @@ func (s *devicesSuite) TestParseDevicesExampleJSONDeviceList(c *gc.C) {
 }
 
 func (s *devicesSuite) TestListDevicesNoFilters(c *gc.C) {
+	s.fakeAPI.SetErrors(
+		nil, // first call to CallGet succeeds
+		nil, // subsequent call to MarshalJSON also succeeds
+		errors.New("you require more vespene gas"), // second call to CallGet fails
+		nil, // third call to CallGet succeds
+		errors.New("we need more pylons"), // but the following MarshalJSON fails
+	)
 	env := s.makeEnviron()
-
-	testProxy := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Hello, client")
-	}))
-	defer testProxy.Close()
-
-	proxyURL, err := url.Parse(testProxy.URL)
-	c.Assert(err, jc.ErrorIsNil)
-
-	s.PatchValue(&s.testMAASObject.TestServer.Server.Config.Handler, httputil.NewSingleHostReverseProxy(proxyURL))
 
 	devices, err := env.listDevices(nil, nil, nil)
 	c.Assert(err, jc.ErrorIsNil)
-	c.Assert(devices, jc.DeepEquals, nil)
+	c.Check(devices, jc.DeepEquals, s.fakeAPI.allDevices)
+
+	devices, err = env.listDevices(nil, nil, nil)
+	c.Assert(err, gc.ErrorMatches, "fetching devices result: you require more vespene gas")
+	c.Check(devices, gc.IsNil)
+
+	devices, err = env.listDevices(nil, nil, nil)
+	c.Assert(err, gc.ErrorMatches, "cannot get JSON bytes: we need more pylons")
+	c.Check(devices, gc.IsNil)
+
+	expectedCall := testing.StubCall{
+		FuncName: "CallGet",
+		Args:     []interface{}{"list", url.Values{}},
+	}
+	s.fakeAPI.CheckCalls(c, []testing.StubCall{expectedCall, expectedCall, expectedCall})
 }
 
 func (s *devicesSuite) TestListDevicesWithSingleFilter(c *gc.C) {
+	env := s.makeEnviron()
 
+	hostFilters := []string{"host1", "host2", "host3"}
+	devices, err := env.listDevices(hostFilters[0:1], nil, nil)
+	c.Assert(err, jc.ErrorIsNil)
+	c.Check(devices, jc.DeepEquals, s.fakeAPI.allDevices)
+
+	devices, err = env.listDevices(hostFilters, nil, nil)
+	c.Assert(err, jc.ErrorIsNil)
+	c.Check(devices, jc.DeepEquals, s.fakeAPI.allDevices)
+
+	s.fakeAPI.CheckCalls(c, []testing.StubCall{{
+		FuncName: "CallGet",
+		Args:     []interface{}{"list", url.Values{"hostname": hostFilters[0:1]}},
+	}, {
+		FuncName: "CallGet",
+		Args:     []interface{}{"list", url.Values{"hostname": hostFilters}},
+	}})
 }
 
-func (s *devicesSuite) TestListDevicesWithMultipleFilters(c *gc.C) {
+func (s *devicesSuite) TestListDevicesWithAllFilters(c *gc.C) {
+	env := s.makeEnviron()
 
+	hostFilters := []string{"host1", "host2", "host3"}
+	parentFilters := []string{"node1", "node2"}
+	idFilters := []string{"id1", "id2", "id3"}
+	devices, err := env.listDevices(hostFilters[0:1], parentFilters[0:1], idFilters[0:1])
+	c.Assert(err, jc.ErrorIsNil)
+	c.Check(devices, jc.DeepEquals, s.fakeAPI.allDevices)
+
+	devices, err = env.listDevices(hostFilters, parentFilters, idFilters)
+	c.Assert(err, jc.ErrorIsNil)
+	c.Check(devices, jc.DeepEquals, s.fakeAPI.allDevices)
+
+	s.fakeAPI.CheckCalls(c, []testing.StubCall{{
+		FuncName: "CallGet",
+		Args: []interface{}{
+			"list",
+			url.Values{
+				"hostname": hostFilters[0:1],
+				"parent":   parentFilters[0:1],
+				"id":       idFilters[0:1],
+			}},
+	}, {
+		FuncName: "CallGet",
+		Args: []interface{}{
+			"list",
+			url.Values{
+				"hostname": hostFilters,
+				"parent":   parentFilters,
+				"id":       idFilters,
+			}},
+	}})
 }
 
 func (s *devicesSuite) TestCreateDeviceRequiresNonEmptyMACAddress(c *gc.C) {
+	env := s.makeEnviron()
 
+	device, err := env.createDevice("", "", "")
+	c.Assert(err, gc.ErrorMatches, "cannot create a device with empty MAC address")
+	c.Check(device, gc.IsNil)
+
+	device, err = env.createDevice("parent", "", "host")
+	c.Assert(err, gc.ErrorMatches, "cannot create a device with empty MAC address")
+	c.Check(device, gc.IsNil)
+
+	s.fakeAPI.CheckNoCalls(c)
 }
 
 func (s *devicesSuite) TestCreateDeviceWithOnlyOptionalParams(c *gc.C) {
+	s.fakeAPI.SetErrors(
+		// first call to CallPost fails
+		errors.New("flawless victory"),
+		nil, // second call to CallPost succeeds
+		errors.New("fatality"), // but the next MarshalJSON call fails
+		nil, // third CallPost succeeds
+		nil, // second MarshalJSON succeeds
+	)
+	env := s.makeEnviron()
 
+	device, err := env.createDevice("", "mac", "")
+	c.Assert(err, gc.ErrorMatches,
+		`cannot create device with parent "", MAC address "mac", hostname "": flawless victory`,
+	)
+	c.Check(device, gc.IsNil)
+
+	device, err = env.createDevice("", "mac", "")
+	c.Assert(err, gc.ErrorMatches, "cannot get JSON bytes: fatality")
+	c.Check(device, gc.IsNil)
+
+	device, err = env.createDevice("", "mac", "")
+	c.Assert(err, jc.ErrorIsNil)
+	c.Check(*device, jc.DeepEquals, s.fakeAPI.singleDevice)
+
+	expectedCall := testing.StubCall{
+		FuncName: "CallPost",
+		Args: []interface{}{
+			"new",
+			url.Values{"mac_addresses": []string{"mac"}},
+		},
+	}
+	s.fakeAPI.CheckCalls(c, []testing.StubCall{expectedCall, expectedCall, expectedCall})
 }
 
 func (s *devicesSuite) TestCreateDeviceWithAllParams(c *gc.C) {
+	env := s.makeEnviron()
 
+	device, err := env.createDevice("parent", "mac", "host")
+	c.Assert(err, jc.ErrorIsNil)
+	c.Check(*device, jc.DeepEquals, s.fakeAPI.singleDevice)
+
+	s.fakeAPI.CheckCalls(c, []testing.StubCall{{
+		FuncName: "CallPost",
+		Args: []interface{}{
+			"new",
+			url.Values{
+				"parent":        []string{"parent"},
+				"mac_addresses": []string{"mac"},
+				"hostname":      []string{"host"},
+			}},
+	}})
 }
 
 func (s *devicesSuite) TestUpdateDeviceWhenNotFound(c *gc.C) {
+	s.fakeAPI.SetErrors(
+		nil,            // first call to GetSubObject succeeds (it can't fail anyway)
+		s.maas404Error, // following call to Update fails with 404 error.
+	)
 
+	env := s.makeEnviron()
+
+	dev := maasDevice{SystemID: "foo"}
+	err := env.updateDevice(dev)
+	c.Assert(err, gc.ErrorMatches, `device "foo" not found`)
+	c.Check(err, jc.Satisfies, errors.IsNotFound)
+
+	s.fakeAPI.CheckCalls(c, []testing.StubCall{{
+		FuncName: "GetSubObject",
+		Args:     []interface{}{"foo"},
+	}, {
+		FuncName: "Update",
+		Args:     []interface{}{url.Values{}},
+	}})
 }
 
 func (s *devicesSuite) TestUpdateDeviceSkipsEmptyParams(c *gc.C) {
+	s.fakeAPI.SetErrors(
+		nil, // first call to GetSubObject succeeds (it can't fail anyway)
+		nil, // following call to Update succeeds
+		nil, // second call to GetSubObject succeeds
+		errors.New("nothing works"), // second call to Update fails
+	)
+	env := s.makeEnviron()
 
+	parent := "parent"
+	dev := maasDevice{
+		SystemID: "foo",
+		Hostname: "bar",
+		ParentID: &parent,
+	}
+	err := env.updateDevice(dev)
+	c.Assert(err, jc.ErrorIsNil)
+
+	dev.ParentID = nil
+	err = env.updateDevice(dev)
+	c.Assert(err, gc.ErrorMatches, `cannot update device "foo": nothing works`)
+
+	s.fakeAPI.CheckCalls(c, []testing.StubCall{{
+		FuncName: "GetSubObject",
+		Args:     []interface{}{"foo"},
+	}, {
+		FuncName: "Update",
+		Args: []interface{}{url.Values{
+			"hostname": []string{"bar"},
+			"parent":   []string{"parent"},
+		}},
+	}, {
+		FuncName: "GetSubObject",
+		Args:     []interface{}{"foo"},
+	}, {
+		FuncName: "Update",
+		Args: []interface{}{url.Values{
+			"hostname": []string{"bar"},
+		}},
+	}})
 }
 
 func (s *devicesSuite) TestDeleteDeviceWhenNotFound(c *gc.C) {
+	s.fakeAPI.SetErrors(
+		nil,            // first call to GetSubObject succeeds (it can't fail anyway)
+		s.maas404Error, // following call to Delete fails with 404 error.
+		nil,            // second call to GetSubObject succeeds
+		errors.New("does not compute"), // second call to Delete fails otherwise.
+	)
+	env := s.makeEnviron()
 
+	err := env.deleteDevice("foo")
+	c.Assert(err, gc.ErrorMatches, `device "foo" not found`)
+	c.Check(err, jc.Satisfies, errors.IsNotFound)
+
+	err = env.deleteDevice("bar")
+	c.Assert(err, gc.ErrorMatches, `cannot delete device "bar": does not compute`)
+
+	s.fakeAPI.CheckCalls(c, []testing.StubCall{{
+		FuncName: "GetSubObject",
+		Args:     []interface{}{"foo"},
+	}, {
+		FuncName: "Delete",
+		Args:     nil,
+	}, {
+		FuncName: "GetSubObject",
+		Args:     []interface{}{"bar"},
+	}, {
+		FuncName: "Delete",
+		Args:     nil,
+	}})
 }
 
 func (s *devicesSuite) TestDeleteDeviceSucceeds(c *gc.C) {
+	env := s.makeEnviron()
 
+	err := env.deleteDevice("valid")
+	c.Assert(err, jc.ErrorIsNil)
+
+	s.fakeAPI.CheckCalls(c, []testing.StubCall{{
+		FuncName: "GetSubObject",
+		Args:     []interface{}{"valid"},
+	}, {
+		FuncName: "Delete",
+		Args:     nil,
+	}})
 }
 
 func (s *devicesSuite) TestGetDeviceWhenNotFound(c *gc.C) {
+	s.fakeAPI.SetErrors(
+		nil,               // first call to GetSubObject succeeds (it can't fail anyway)
+		s.maas404Error,    // following call to Get fails with 404 error.
+		nil,               // second call to GetSubObject succeeds
+		errors.New("N/A"), // second call to Get fails otherwise.
+	)
+	env := s.makeEnviron()
 
+	device, err := env.getDevice("widget")
+	c.Assert(err, gc.ErrorMatches, `device "widget" not found`)
+	c.Check(err, jc.Satisfies, errors.IsNotFound)
+	c.Check(device, gc.IsNil)
+
+	device, err = env.getDevice("X")
+	c.Assert(err, gc.ErrorMatches, `cannot get device "X": N/A`)
+	c.Check(device, gc.IsNil)
+
+	s.fakeAPI.CheckCalls(c, []testing.StubCall{{
+		FuncName: "GetSubObject",
+		Args:     []interface{}{"widget"},
+	}, {
+		FuncName: "Get",
+		Args:     nil,
+	}, {
+		FuncName: "GetSubObject",
+		Args:     []interface{}{"X"},
+	}, {
+		FuncName: "Get",
+		Args:     nil,
+	}})
 }
 
 func (s *devicesSuite) TestGetDeviceSucceeds(c *gc.C) {
+	env := s.makeEnviron()
 
+	device, err := env.getDevice("boo")
+	c.Assert(err, jc.ErrorIsNil)
+	c.Check(*device, jc.DeepEquals, s.fakeAPI.singleDevice)
+
+	s.fakeAPI.CheckCalls(c, []testing.StubCall{{
+		FuncName: "GetSubObject",
+		Args:     []interface{}{"boo"},
+	}, {
+		FuncName: "Get",
+		Args:     nil,
+	}})
 }
 
 func (s *devicesSuite) TestSetupDeviceInterfacesEmptyConfig(c *gc.C) {
@@ -252,28 +527,58 @@ func (s *devicesSuite) TestClaimStickyIPForDeviceWithExplicitAddress(c *gc.C) {
 type fakeDevicesAPI struct {
 	*testing.Stub
 
-	callGetResult  interface{}
-	callPostResult interface{}
-	updateResult   interface{}
-	getResult      interface{}
+	singleDevice maasDevice
+	allDevices   []maasDevice
 }
 
 var (
-	_ devicesAPI = (*gomaasapi.MAASObject)(nil)
+	_ devicesAPI = (*devicesAPIShim)(nil)
 	_ devicesAPI = (*fakeDevicesAPI)(nil)
 )
 
-func (f *fakeDevicesAPI) GetSubObject(uri string) devicesAPI {
-	f.Stub.AddCall("GetSubObject", uri)
-	f.Stub.PopNoErr()
-	return f
+func (f *fakeDevicesAPI) addCallAndGetResultOrError(callName string, args ...interface{}) (devicesAPI, error) {
+	f.AddCall(callName, args...)
+	if err := f.NextErr(); err != nil {
+		return nil, err
+	}
+	return f, nil
 }
 
-func (f *fakeDevicesAPI) CallGet(op string, params url.Values) (gomaasapi.JSONObject, error) {
-	f.Stub.AddCall("CallGet", op, params)
-	return gomaasapi.JSONObjectFromStruct()
-	if err := f.Stub.NextErr(); err != nil {
-		return gomaasapi.JSONObject{}, err
+func (f *fakeDevicesAPI) GetSubObject(uri string) devicesAPI {
+	result, _ := f.addCallAndGetResultOrError("GetSubObject", uri)
+	return result
+}
+
+func (f *fakeDevicesAPI) CallGet(op string, params url.Values) (devicesAPI, error) {
+	return f.addCallAndGetResultOrError("CallGet", op, params)
+}
+
+func (f *fakeDevicesAPI) CallPost(op string, params url.Values) (devicesAPI, error) {
+	return f.addCallAndGetResultOrError("CallPost", op, params)
+}
+
+func (f *fakeDevicesAPI) Delete() error {
+	_, err := f.addCallAndGetResultOrError("Delete")
+	return err
+}
+
+func (f *fakeDevicesAPI) Update(params url.Values) (devicesAPI, error) {
+	return f.addCallAndGetResultOrError("Update", params)
+}
+
+func (f *fakeDevicesAPI) Get() (devicesAPI, error) {
+	return f.addCallAndGetResultOrError("Get")
+}
+
+func (f *fakeDevicesAPI) MarshalJSON() ([]byte, error) {
+	if err := f.NextErr(); err != nil {
+		return nil, err
 	}
-	return f
+
+	if calls := f.Calls(); len(calls) > 0 && calls[len(calls)-1].FuncName == "CallGet" {
+		// Only CallGet can return more than one result, if it was the last call
+		// made.
+		return json.Marshal(f.allDevices)
+	}
+	return json.Marshal(f.singleDevice)
 }
