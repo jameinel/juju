4a5eeb219f551cbb6d646f554e50e2b6ddcdce6c
Dimiter Naydenov <dimiter.naydenov@canonical.com>
2016-02-03 19:52:03 +0200

    state: Initial tests for the refactored endpoint bindings code

--- state/endpoint_bindings_test.go
+++ state/endpoint_bindings_test.go
@@ -1,225 +1,84 @@
 // Copyright 2015 Canonical Ltd.
 // Licensed under the AGPLv3, see LICENCE file for details.
 
-package state_test
+package state
 
 import (
+	"github.com/juju/testing"
 	jc "github.com/juju/testing/checkers"
 	gc "gopkg.in/check.v1"
-	"gopkg.in/juju/charm.v6-unstable"
-
-	"github.com/juju/juju/network"
-	"github.com/juju/juju/state"
+	"gopkg.in/mgo.v2/bson"
 )
 
-type BindingsSuite struct {
-	ConnSuite
+type bindingsMapSuite struct {
+	testing.IsolationSuite
 
-	oldMeta     *charm.Meta
-	oldDefaults map[string]string
-	newMeta     *charm.Meta
-	newDefaults map[string]string
+	escapedMapWithBadKeys   map[string]string
+	unescapedMapWithBadKeys bindingsMap
 }
 
-var _ = gc.Suite(&BindingsSuite{})
+var _ = gc.Suite(&bindingsMapSuite{})
 
-func (s *BindingsSuite) SetUpTest(c *gc.C) {
-	// TODO(dimitern): This test needs fixing before merging into master.
-	c.Skip("skipped temporarily to pass CI merge gating")
+func (s *bindingsMapSuite) SetUpTest(c *gc.C) {
+	s.IsolationSuite.SetUpTest(c)
 
-	s.ConnSuite.SetUpTest(c)
-
-	const dummyCharmWithOneOfEachRelationType = `
-name: dummy
-summary: "That's a dummy charm with one relation of each type."
-description: "This is a longer description."
-provides:
-  foo1:
-    interface: phony
-requires:
-  bar1:
-    interface: fake
-peers:
-  self:
-    interface: dummy
-`
-	oldCharm := s.AddMetaCharm(c, "dummy", dummyCharmWithOneOfEachRelationType, 1)
-	s.oldMeta = oldCharm.Meta()
-	s.oldDefaults = map[string]string{
-		"foo1": network.DefaultSpace,
-		"bar1": network.DefaultSpace,
-		"self": network.DefaultSpace,
+	s.escapedMapWithBadKeys = map[string]string{
+		"bad" + fullWidthDollar + "key": "must be unescaped",
+		"also " + fullWidthDot + " bad": "same thing",
+		fullWidthDot:                    "even by itself",
+		fullWidthDollar + fullWidthDot:  "or in combination",
 	}
 
-	const dummyCharmWithTwoOfEachRelationType = `
-name: dummy
-summary: "That's a dummy charm with 2 relations for each type."
-description: "This is a longer description."
-provides:
-  foo1:
-    interface: phony
-  foo2:
-    interface: secret
-requires:
-  bar2: real
-  bar3:
-    interface: cool
-peers:
-  self:
-    interface: dummy
-  me: peer
-`
-	newCharm := s.AddMetaCharm(c, "dummy", dummyCharmWithTwoOfEachRelationType, 2)
-	s.newMeta = newCharm.Meta()
-	s.newDefaults = map[string]string{
-		"foo1": network.DefaultSpace,
-		"foo2": network.DefaultSpace,
-		"bar2": network.DefaultSpace,
-		"bar3": network.DefaultSpace,
-		"self": network.DefaultSpace,
-		"me":   network.DefaultSpace,
+	s.unescapedMapWithBadKeys = bindingsMap{
+		"bad$key":    "must be unescaped",
+		"also . bad": "same thing",
+		".":          "even by itself",
+		"$.":         "or in combination",
 	}
+}
 
-	// Add some spaces to use in bindings, but notably NOT the default space, as
-	// it should be always allowed.
-	_, err := s.State.AddSpace("client", "", nil, true)
+func (s *bindingsMapSuite) TestSetBSONReportsUnmarshalErrors(c *gc.C) {
+	emptyMap := make(bindingsMap)
+	emptyRaw := bson.Raw{}
+
+	err := emptyMap.SetBSON(emptyRaw)
+	c.Assert(err, gc.NotNil)
+	c.Assert(err, gc.ErrorMatches, "Unknown element kind .*")
+}
+
+func (s *bindingsMapSuite) TestSetBSONUnescapesSpecialCharactersInKeys(c *gc.C) {
+	bsonBytes, err := bson.Marshal(s.escapedMapWithBadKeys)
 	c.Assert(err, jc.ErrorIsNil)
-	_, err = s.State.AddSpace("apps", "", nil, false)
+
+	rawBSONWithBadKeys := bson.Raw{
+		Kind: 0x03, // Document
+		Data: bsonBytes,
+	}
+
+	inputMap := make(bindingsMap)
+	err = inputMap.SetBSON(rawBSONWithBadKeys)
 	c.Assert(err, jc.ErrorIsNil)
+	c.Assert(inputMap, jc.DeepEquals, s.unescapedMapWithBadKeys)
 }
 
-func (s *BindingsSuite) TestMergeBindings(c *gc.C) {
-	// TODO(dimitern): This test needs fixing before merging into master.
-	c.Skip("skipped temporarily to pass CI merge gating")
+func (s *bindingsMapSuite) TestGetBSONReturnsNonNilMapWithEmptyOrNilInput(c *gc.C) {
+	var nilMap bindingsMap
+	emptyMap := make(bindingsMap)
 
-	// The test cases below are not exhaustive, but just check basic
-	// functionality. Most of the logic is tested by calling service.SetCharm()
-	// in various ways.
-	controllerSpace := "controllers"
-	for i, test := range []struct {
-		about          string
-		newMap, oldMap map[string]string
-		meta           *charm.Meta
-		updated        map[string]string
-		removed        []string
-	}{{
-		about:   "defaults used when both newMap and oldMap are nil",
-		newMap:  nil,
-		oldMap:  nil,
-		meta:    s.oldMeta,
-		updated: s.copyMap(s.oldDefaults),
-		removed: nil,
-	}, {
-		about:  "oldMap overrides defaults, newMap is nil",
-		newMap: nil,
-		oldMap: map[string]string{
-			"foo1": "client",
-			"self": "db",
-		},
-		meta: s.oldMeta,
-		updated: map[string]string{
-			"foo1": "client",
-			"bar1": controllerSpace,
-			"self": "db",
-		},
-		removed: nil,
-	}, {
-		about: "oldMap overrides defaults, newMap overrides oldMap",
-		newMap: map[string]string{
-			"foo1": controllerSpace,
-			"self": "db",
-			"bar1": "client",
-		},
-		oldMap: map[string]string{
-			"foo1": "client",
-			"bar1": "db",
-		},
-		meta: s.oldMeta,
-		updated: map[string]string{
-			"foo1": controllerSpace,
-			"bar1": "client",
-			"self": "db",
-		},
-		removed: nil,
-	}, {
-		about: "newMap overrides defaults, oldMap is nil",
-		newMap: map[string]string{
-			"self": "db",
-		},
-		oldMap: nil,
-		meta:   s.oldMeta,
-		updated: map[string]string{
-			"foo1": controllerSpace,
-			"bar1": controllerSpace,
-			"self": "db",
-		},
-		removed: nil,
-	}, {
-		about:  "obsolete entries in oldMap missing in defaults are removed",
-		newMap: nil,
-		oldMap: map[string]string{
-			"any-old-thing": "boo",
-			"self":          "db",
-		},
-		meta: s.oldMeta,
-		updated: map[string]string{
-			"foo1": controllerSpace,
-			"bar1": controllerSpace,
-			"self": "db",
-		},
-		removed: []string{"any-old-thing"},
-	}, {
-		about: "new endpoints use defaults unless specified in newMap, existing ones are kept",
-		newMap: map[string]string{
-			"foo2": "db",
-			"me":   "client",
-			"bar3": "db",
-		},
-		oldMap: s.copyMap(s.oldDefaults),
-		meta:   s.newMeta,
-		updated: map[string]string{
-			"foo1": controllerSpace,
-			"foo2": "db",
-			"bar2": controllerSpace,
-			"bar3": "db",
-			"self": controllerSpace,
-			"me":   "client",
-		},
-		removed: []string{"bar1"},
-	}} {
-		c.Logf("test #%d: %s", i, test.about)
-
-		// TODO(dimitern): Temporarily disabled along with the whole suite to unblock merging into master
-		//updated, removed, err := state.MergeBindings(test.newMap, test.oldMap, test.meta, controllerSpace)
-		//c.Check(err, jc.ErrorIsNil)
-		//c.Check(updated, jc.DeepEquals, test.updated)
-		//c.Check(removed, jc.DeepEquals, test.removed)
+	for _, inputMap := range []bindingsMap{nilMap, emptyMap} {
+		result, err := inputMap.GetBSON()
+		c.Check(err, jc.ErrorIsNil)
+		c.Check(result, gc.NotNil)
+		c.Check(result, gc.FitsTypeOf, bindingsMap{})
 	}
 }
 
-func (s *BindingsSuite) TestCombinedCharmRelations(c *gc.C) {
-	_, err := state.CombinedCharmRelations(nil)
-	c.Check(err, gc.ErrorMatches, "nil charm metadata")
+func (s *bindingsMapSuite) TestGetBSONEscapesSpecialCharactersInKeys(c *gc.C) {
+	result, err := s.unescapedMapWithBadKeys.GetBSON()
+	c.Assert(err, jc.ErrorIsNil)
+	c.Assert(result, gc.NotNil)
+	c.Assert(result, gc.FitsTypeOf, map[string]string{})
 
-	meta := s.newMeta
-	allRelations, err := state.CombinedCharmRelations(meta)
-	c.Check(err, jc.ErrorIsNil)
-	combinedLength := len(meta.Provides) + len(meta.Requires) + len(meta.Peers)
-	c.Check(allRelations, gc.HasLen, combinedLength)
-	c.Check(allRelations, jc.DeepEquals, map[string]charm.Relation{
-		"foo1": meta.Provides["foo1"],
-		"foo2": meta.Provides["foo2"],
-		"bar2": meta.Requires["bar2"],
-		"bar3": meta.Requires["bar3"],
-		"self": meta.Peers["self"],
-		"me":   meta.Peers["me"],
-	})
-}
-
-func (s *BindingsSuite) copyMap(input map[string]string) map[string]string {
-	output := make(map[string]string, len(input))
-	for key, value := range input {
-		output[key] = value
-	}
-	return output
+	escapedMap := result.(map[string]string)
+	c.Assert(escapedMap, jc.DeepEquals, s.escapedMapWithBadKeys)
 }
