895078bfcd61ead020c0e0c58a9a8d2525ff1af9
Dimiter Naydenov <dimiter.naydenov@canonical.com>
2016-04-12 19:14:35 +0300

    ec2: Fixed address-allocation code to add interface type for NICs

--- provider/ec2/environ.go
+++ provider/ec2/environ.go
@@ -1026,6 +1026,7 @@ func (e *environ) NetworkInterfaces(instId instance.Id) ([]network.InterfaceInfo
 			Disabled:      false,
 			NoAutoStart:   false,
 			ConfigType:    network.ConfigDHCP,
+			InterfaceType: network.EthernetInterface,
 			Address:       network.NewScopedAddress(iface.PrivateIPAddress, network.ScopeCloudLocal),
 		}
 	}
